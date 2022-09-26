daa7a1fc4ae36a639014b4067eae6191ede7f795
CarlosjuniorBr1 <carlos.alberto369@outlook.com>
2022-09-26 16:17:35 -0300

    Primeira parte da Aplicação Console. Foram criadas as classes: Worker, HourContract, Department, WorkerLevel

---
+++ Entities/WorkerBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Course.Entities
+{
+    internal class WorkerBase
+    {
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue;
+                }
+
+            }
+        }
+    }
+}
